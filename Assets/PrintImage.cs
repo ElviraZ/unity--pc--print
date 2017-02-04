0bddd6ce8029df91c62206f3e1c239e0c2c16444
Elvira_Z <502768095@qq.com>
2017-02-04 17:21:22 +0800

    unity  调用pc  打印机
    unity  调用pc  打印机

---
+++ Assets/PrintImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintImage : MonoBehaviour
+{
+ 
+    public void PrintFile()
+    {
+        PrintFiles();
+    }
+   /// <summary>
+    /// 打印
+    /// </summary>
+     void PrintFiles(string path=null)
+    {
+        path = "d:\\screenshots\\sample.jpg";
+        System.Diagnostics.Process process = new System.Diagnostics.Process(); //系统进程
+        process.StartInfo.CreateNoWindow = false; //不显示调用程序窗口
+        process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;//
+        process.StartInfo.UseShellExecute = true; //采用操作系统自动识别模式
+        process.StartInfo.FileName = path; //要打印的文件路径
+     
+        process.StartInfo.Verb = "print"; //指定对图片执行的动作，打印：print   打开：open …………
+        process.Start(); //开始打印
+    }
+}
